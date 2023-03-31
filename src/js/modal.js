47fbbd0c146b2626034db1655e830df3f02dba3d
Jonathan Juarez <jonathan.juarezwp@gmail.com>
2023-03-31 09:45:35 -0600

    Add modal.js file

---
+++ src/js/modal.js
@@ -0,0 +1,10 @@
+import { Fancybox } from '@fancyapps/ui/dist/fancybox/fancybox.esm.js'
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  /* Fancybox.show([
+    {
+      src: '/assets/images/Alexion-Logo.png',
+      caption: 'Single image'
+    }
+  ]) */
+})
