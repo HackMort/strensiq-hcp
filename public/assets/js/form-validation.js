28f9b0402fda854871eedbd62a04631735202d39
Anderson Castellon <anderson.ivan.fuentes@gmail.com>
2023-04-17 15:48:31 -0600

    🐛 Resolve comments about form

--- public/assets/js/form-validation.js
+++ public/assets/js/form-validation.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-unused-vars */
+const parentClassName = 'form__control'
+const formClassName = 'form'
 /**
      * If the parent element of the child element has the class name, return the parent
      * element, otherwise, call the function again with the parent element as the child
@@ -15,6 +17,37 @@ const getParent = function (child, className) {
     return parent
   } else {
     return getParent(parent, className)
+  }
+}
+
+/**
+     * If any of the radio buttons in the group are checked, return true.
+     * @param name {string} - The name of the radio group.
+     * @returns {boolean} A boolean value.
+     */
+const radioGroupIsValid = function (name) {
+  const radioGroup = document.getElementsByName(`input[type="radio"][name=${name}]`)
+  let isValid = false
+  for (let i = 0; i < radioGroup.length; i++) {
+    if (radioGroup[i].checked) {
+      isValid = true
+      break
+    }
+  }
+
+  return isValid
+}
+
+/**
+ * The function sets a dataset attribute to "true" for a given form control to indicate that it has
+ * been touched.
+ * @param {HTMLDivElement} control - The `control` parameter is a form control element, such as an input, select, or
+ * textarea, that needs to be marked as touched. The function `markFormControlAsTouched` sets the
+ * `data-touched` attribute of the control to `'true'`, indicating that the control has been touched
+ */
+const markFormControlAsTouched = (control) => {
+  if (control) {
+    control.dataset.touched = 'true'
   }
 }
 
@@ -45,41 +78,52 @@ const checkFormValidity = function (form) {
     .every((control) => control.dataset.isValid === 'true')
 }
 
-function validateFormControl (event) {
-  const parentClassName = 'form__control'
-  const formClassName = 'form'
+/**
+ * The function sets the validation status of a form control based on its required status and validity
+ * of its native control.
+ * @param {HTMLDivElement} formControl - The HTML element that represents the form control, such as an input or select
+ * element.
+ * @param {HTMLInputElement | HTMLSelectElement} nativeControl - The nativeControl parameter is a reference to the HTML input element that the
+ * validation status is being set for.
+ */
+const setValidationStatusToFormControl = (formControl, nativeControl) => {
+  const isOptional = formControl.dataset.required === 'false'
+  markFormControlAsTouched(formControl)
 
-  /**
-     * If any of the radio buttons in the group are checked, return true.
-     * @param name {string} - The name of the radio group.
-     * @returns {boolean} A boolean value.
-     */
-  const radioGroupIsValid = function (name) {
-    const radioGroup = document.getElementsByName(`input[type="radio"][name=${name}]`)
-    let isValid = false
-    for (let i = 0; i < radioGroup.length; i++) {
-      if (radioGroup[i].checked) {
-        isValid = true
-        break
-      }
+  if (nativeControl.type === 'radio' && !nativeControl.checked) {
+    const groupIsValid = radioGroupIsValid(nativeControl.name)
+
+    if (groupIsValid || isOptional) {
+      formControl.classList.remove('form__control--invalid')
+      formControl.dataset.isValid = 'true'
+    } else {
+      formControl.classList.add('form__control--invalid')
+      formControl.dataset.isValid = 'false'
     }
-
-    return isValid
+  } else {
+    if (nativeControl.checkValidity() || isOptional) {
+      formControl.classList.remove('form__control--invalid')
+      formControl.dataset.isValid = 'true'
+    } else {
+      formControl.classList.add('form__control--invalid')
+      formControl.dataset.isValid = 'false'
+    }
   }
+}
 
-  const control = event.target
-  const parent = getParent(control, parentClassName)
-  const form = getParent(parent, formClassName)
+function validateFormControl (event) {
+  const nativeControl = event.target
+  const formControl = getParent(nativeControl, parentClassName)
+  const form = getParent(formControl, formClassName)
   const submitButton = form.querySelector("[type='submit']")
-  const controls = Array.from(form.querySelectorAll('.' + parentClassName))
 
-  parent.dataset.touched = 'true'
+  markFormControlAsTouched(formControl)
 
   /**
 
-    Disables a submit button by adding the 'disabled' attribute and the 'disabled' class to its parent element.
-    @param {Element} submit - The submit button to be disabled.
-    */
+      Disables a submit button by adding the 'disabled' attribute and the 'disabled' class to its parent element.
+      @param {Element} submit - The submit button to be disabled.
+      */
   const disableSubmitButton = (submit) => {
     submit && submit.setAttribute('disabled', 'true')
     if (submit.parentElement) {
@@ -89,9 +133,9 @@ function validateFormControl (event) {
 
   /**
 
-    Enables a submit button by removing the 'disabled' attribute and the 'disabled' class from its parent element.
-    @param {Element} submit - The submit button to be enabled.
-    */
+      Enables a submit button by removing the 'disabled' attribute and the 'disabled' class from its parent element.
+      @param {Element} submit - The submit button to be enabled.
+      */
   const enableSubmitButton = (submit) => {
     submit && submit.removeAttribute('disabled')
     if (submit.parentElement) {
@@ -100,9 +144,9 @@ function validateFormControl (event) {
   }
 
   /**
-     * If the form is valid, remove the disabled attribute from the submit button,
-     * otherwise add the disabled attribute to the submit button
-     */
+       * If the form is valid, remove the disabled attribute from the submit button,
+       * otherwise add the disabled attribute to the submit button
+       */
   const toggleSubmitButtonState = function () {
     if (form && checkFormValidity(form)) {
       enableSubmitButton(submitButton)
@@ -115,29 +159,25 @@ function validateFormControl (event) {
     }
   }
 
-  toggleSubmitButtonState()
+  // toggleSubmitButtonState()
 
-  const isOptional = parent.dataset.required === 'false'
+  setValidationStatusToFormControl(formControl, nativeControl)
 
-  if (control.type === 'radio' && !control.checked) {
-    const groupIsValid = radioGroupIsValid(control.name)
+  // toggleSubmitButtonState()
+}
 
-    if (groupIsValid || isOptional) {
-      parent.classList.remove('form__control--invalid')
-      parent.dataset.isValid = 'true'
-    } else {
-      parent.classList.add('form__control--invalid')
-      parent.dataset.isValid = 'false'
-    }
-  } else {
-    if (control.checkValidity() || isOptional) {
-      parent.classList.remove('form__control--invalid')
-      parent.dataset.isValid = 'true'
-    } else {
-      parent.classList.add('form__control--invalid')
-      parent.dataset.isValid = 'false'
-    }
+function validateFormOnSubmit (event) {
+  const form = event.target.form
+  const formIsValid = checkFormValidity(form)
+
+  if (!formIsValid) {
+    event.preventDefault()
+
+    const controls = Array.from(form.querySelectorAll('.' + parentClassName))
+    controls.forEach((formControl) => {
+      const nativeControl = formControl.querySelector('input') || formControl.querySelector('select')
+
+      setValidationStatusToFormControl(formControl, nativeControl)
+    })
   }
-
-  toggleSubmitButtonState()
 }
