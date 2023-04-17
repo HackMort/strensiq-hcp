446342d48c7dcc32afc89a610bda48ead084dfbf
Jhossymar Contreras <christian.contreras@jdoutstanding.com>
2023-04-17 10:28:40 -0600

    refactor: :recycle: Fix script skeleton for desktop version
    Update code and fix functions for skeleton component working on desktop version in a correct way
    
    \

--- src/js/skeleton.js
+++ src/js/skeleton.js
@@ -72,74 +72,143 @@ document.addEventListener('DOMContentLoaded', event => {
 
   /* Loop through the array by adding the click event on each element */
   skeletonWords.forEach(skeletonWord => {
-    skeletonWord.addEventListener('click', event => {
-      const wordParent = event.target.closest('.skeleton__point')
-      const symptomsList = wordParent.closest('.skeleton__image-container').querySelector('.skeleton__symptoms-list')
-
-      /* If word is not in an active state hide previous and change this element to active */
-      if (!wordParent.classList.contains('skeleton__point--active')) {
-        hiddenSymponstList()
-        const dataWord = wordParent.dataset.word
-        const dataListPosition = wordParent.dataset.listPosition
-        calculateListPosition(symptomsList, dataListPosition, wordParent)
-        setHeightAfterElement(symptomsList, wordParent)
-        const symptoms = symptomsData.find(data => {
-          return data.mainWord === dataWord
-        })
-        symptomsList.innerHTML = symptoms.symptoms.map(symptom => {
-          return `<li class="skeleton__symptom">${symptom}</li>`
-        }).join('')
-        showSymptonsList(symptomsList, wordParent)
-      } else {
-        hiddenSymponstList()
-      }
-    })
+    skeletonWord.addEventListener('click', wordClickEventHandler)
   })
 })
 
-function hiddenSymponstList (symptomsList) {
+async function wordClickEventHandler (event) {
+  const pointElement = event.target.closest('.skeleton__point')
+  const symptomsList = pointElement.closest('.skeleton__image-container').querySelector('.skeleton__symptoms-list')
+
+  /* If point is not on an active state hide previous and change this element to active */
+  const isThisPointActive = pointElement.classList.contains('skeleton__point--active')
+  await hiddenSymponstList(!isThisPointActive)
+
+  if (!isThisPointActive) {
+    /* Calculate position of symptoms list, length of vertical and horizontal lines from point */
+    calculateListPosition(symptomsList, pointElement)
+    setLengthAfterElement(symptomsList, pointElement)
+    setLengthBeforeElement(symptomsList, pointElement)
+
+    /* Get and set data of symptoms according to the selected word */
+    const dataWord = pointElement.dataset.word
+    const symptoms = symptomsData.find(data => {
+      return data.mainWord === dataWord
+    })
+    symptomsList.innerHTML = symptoms.symptoms.map(symptom => {
+      return `<li class="skeleton__symptom">${symptom}</li>`
+    }).join('')
+
+    showSymptonsList(symptomsList, pointElement)
+  }
+}
+
+/* Hide the symptoms list, if this function is fired by the current active element
+awaits 500ms to let the fade out animation runs */
+async function hiddenSymponstList (isAnotherElementActive = true) {
   const activeElement = document.querySelector('.skeleton__point--active')
   if (activeElement) {
     activeElement.classList.remove('skeleton__point--active')
 
     const symptomsList = activeElement.closest('.skeleton__image-container').querySelector('.skeleton__symptoms-list')
     if (!symptomsList.classList.contains('skeleton__symptoms-list--hidden')) {
-      symptomsList.classList.add('skeleton__symptoms-list--hidden')
+      symptomsList.classList.remove('in-animation')
+      symptomsList.classList.add('out-animation')
+      if (isAnotherElementActive) {
+        symptomsList.classList.add('skeleton__symptoms-list--hidden')
+      } else {
+        setTimeout(() => {
+          symptomsList.classList.add('skeleton__symptoms-list--hidden')
+        }, 500)
+      }
     }
   }
 }
 
-function showSymptonsList (symptomsList, element) {
+function showSymptonsList (symptomsList, pointElement) {
   symptomsList.classList.remove('skeleton__symptoms-list--hidden')
-  element.classList.add('skeleton__point--active')
+  symptomsList.classList.remove('out-animation')
+  symptomsList.classList.add('in-animation')
+  pointElement.classList.add('skeleton__point--active')
 }
 
-function calculateListPosition (symptomsList, listPosition, pointElement = null) {
-  const distanceUnderPointElement = 30
-  let leftWordPosition = 0
+function calculateListPosition (symptomsList, pointElement) {
+  let distanceFromPointElement = 30
+  const listPosition = pointElement.dataset.listPosition
 
-  if (listPosition === 'mobile-top') {
-    pointElement = document.querySelector('.skeleton__point[data-word="dental"]')
-  } else if (listPosition === 'mobile-bottom') {
-    pointElement = document.querySelector('.skeleton__point[data-word="muscular"]')
+  if (isSkeletonMobile(pointElement)) {
+    if (listPosition === 'mobile-top') {
+      pointElement = document.querySelector('.skeleton__point[data-word="dental"]')
+    } else if (listPosition === 'mobile-bottom') {
+      pointElement = document.querySelector('.skeleton__point[data-word="muscular"]')
+    }
+
+    const pointElementStyles = getComputedStyle(pointElement)
+    const pointElementTop = parseInt(pointElementStyles.getPropertyValue('top').slice(0, -2))
+    const listTopPosition = pointElementTop + distanceFromPointElement
+    symptomsList.style.setProperty('top', listTopPosition + 'px')
   } else {
-    if(pointElement.classList.contains('skeleton__pooint--invert'))
-    sideWordPosition = pointElement.clientWidth
-    symptomsList.style.setProperty('left', sideWordPosition + "px")
+    distanceFromPointElement = 35
+    const horizontalPosition = 'left'
+    const dataVerticalLine = pointElement.dataset.verticalLine === 'top' ? 'bottom' : 'top'
+    const pointElementStyles = getComputedStyle(pointElement)
+    const wordElement = pointElement.querySelector('.skeleton__word')
+    const wordElementStyles = getComputedStyle(wordElement)
+    const pointElementPosition = parseInt(pointElementStyles.getPropertyValue(horizontalPosition).slice(0, -2))
+    const wordElementPosition = parseInt(wordElementStyles.getPropertyValue(horizontalPosition).slice(0, -2))
+    const symptomsListPosition = pointElementPosition + wordElementPosition
+    const pointElementVerticalPosition = parseInt(pointElementStyles.getPropertyValue(dataVerticalLine).slice(0, -2))
+    const listVerticalPosition = pointElementVerticalPosition + distanceFromPointElement
+
+    symptomsList.style.setProperty('right', 'unset')
+    symptomsList.style.setProperty('left', 'unset')
+    symptomsList.style.setProperty('top', 'unset')
+    symptomsList.style.setProperty('bottom', 'unset')
+    symptomsList.style.setProperty(horizontalPosition, symptomsListPosition + 'px')
+    symptomsList.style.setProperty(dataVerticalLine, listVerticalPosition + 'px')
   }
-  // setHeightAfterElement(pointElement)
-  const pointElementStyles = getComputedStyle(pointElement)
-  const pointElementTop = parseInt(pointElementStyles.getPropertyValue('top').slice(0, -2))
-  const listTopPosition = pointElementTop + distanceUnderPointElement
-  symptomsList.style.setProperty('top', listTopPosition + 'px')
 }
 
-function setHeightAfterElement (symptomsList, element) {
-  const wordParentStyles = getComputedStyle(element)
-  const symptomsListStyles = getComputedStyle(symptomsList)
-  const wordParentTop = parseInt(wordParentStyles.getPropertyValue('top').slice(0, -2))
-  const symptomsListTop = parseInt(symptomsListStyles.getPropertyValue('top').slice(0, -2))
+function setLengthAfterElement (symptomsList, pointElement) {
+  if (isSkeletonMobile(pointElement)) {
+    const wordParentStyles = getComputedStyle(pointElement)
+    const symptomsListStyles = getComputedStyle(symptomsList)
+    const wordParentTop = parseInt(wordParentStyles.getPropertyValue('top').slice(0, -2))
+    const symptomsListTop = parseInt(symptomsListStyles.getPropertyValue('top').slice(0, -2))
 
-  const wordParentAfterHeight = Math.abs(symptomsListTop - wordParentTop)
-  element.style.setProperty('--point-active-after-height', wordParentAfterHeight + 'px')
+    const wordParentAfterLength = Math.abs(symptomsListTop - wordParentTop)
+    pointElement.style.setProperty('--point-active-after-height', wordParentAfterLength + 'px')
+  } else {
+    const dataVerticalLine = pointElement.dataset.verticalLine
+    if (dataVerticalLine === 'top') {
+      pointElement.style.setProperty('--point-active-after-height', '115px')
+    } else {
+      pointElement.style.setProperty('--point-active-after-height', '15px')
+    }
+  }
+}
+
+function setLengthBeforeElement (symptomsList, pointElement) {
+  if (!isSkeletonMobile(pointElement)) {
+    const dataWordAlignment = pointElement.dataset.wordAlignment
+    const horizontalPosition = 'left'
+    const wordParentStyles = getComputedStyle(pointElement)
+    const symptomsListStyles = getComputedStyle(symptomsList)
+    const wordParentPosition = parseInt(wordParentStyles.getPropertyValue(horizontalPosition).slice(0, -2))
+    const symptomsListPosition = parseInt(symptomsListStyles.getPropertyValue(horizontalPosition).slice(0, -2))
+
+    let wordParentBeforeLenght = 0
+    if (dataWordAlignment === 'left') {
+      wordParentBeforeLenght = Math.abs(symptomsListPosition - wordParentPosition) - 150
+    } else {
+      wordParentBeforeLenght = Math.abs(symptomsListPosition - wordParentPosition)
+    }
+
+    pointElement.style.setProperty('--point-active-before-width', wordParentBeforeLenght + 'px')
+  }
+}
+
+function isSkeletonMobile (skeletonChildren) {
+  const skeletonParent = skeletonChildren.closest('.skeleton')
+  return skeletonParent.classList.contains('skeleton--mobile')
 }
