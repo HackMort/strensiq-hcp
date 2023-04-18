f9ebf290d0ae9ca66086a379525d1e9363c65865
elian-dev <geordymemdoza@gmail.com>
2023-04-18 09:38:12 -0600

    feat: :construction: Create table component

---
+++ src/pages/clinical-data/data/safety.js
@@ -0,0 +1,41 @@
+//Table data
+const data = [
+    {
+      text: ["Injection site reactions", "38(58)", "6(46)", "44(56)", "18(90)"],
+      classModifier: ["mh"],
+    },
+    {
+        text: ["Erythema", "29(44)", "3(23)", "23(41)", "15(75)"],
+    },
+    {
+        text: ["Discoloration/hypopigmentation", "11(17)", "1(8)", "8(13)", "5(25)"],
+    },
+    {
+        text: ["Pain", "10(15)", "1(8)", "7(11)", "4(20)"],
+    },
+    {
+        text: ["Swelling", "9(14)", "1(8)", "6(10)", "4(20)"],
+    },
+    {
+        text: ["Induration", "8(12)", "1(8)", "5(8)", "3(15)"],
+    },
+    {
+        text: ["Pruritus", "7(11)", "1(8)", "4(6)", "3(15)"],
+    },
+    {
+        text: ["Burning", "6(9)", "1(8)", "3(5)", "2(10)"],
+        classModifier: ["mh"],
+    },
+    {
+        text: ["Tenderness", "6(9)", "1(8)", "3(5)", "2(10)"],
+    },
+    {
+        text: ["Numbness", "5(8)", "1(8)", "2(3)", "2(10)"],
+    },
+    {
+        text: ["Paresthesia", "5(8)", "1(8)", "2(3)", "2(10)"],
+    },
+
+];
+
+export default data;
---
+++ src/pages/clinical-data/data/study.js
@@ -0,0 +1,21 @@
+//Table data
+const studyData = [
+    {
+      text: ["Age at inclusion", "≤3 years", "≤5 years", "6-12 years", "≥13 years"],
+      classModifier: ["mh"],
+    },
+    {
+        text: ["Description", "Prospective single-arm-trial and extension", "Prospective open-label trial", "Prospective open-label trial and extension", "Multicenter, randomized, open-label concurrent trial"],
+        classModifier: ["mh"],
+    },
+    {
+        text: ["Duration", "7 years", "6 years", "7 years", "5 years"],
+        classModifier: ["mh", "mb"],
+    },
+    {
+        text: ["N", "11", "69", "19(9 reaches adolescence during the study", "19(13 adults,c 6 adolescents b)"],
+        classModifier: ["mh"],
+    },
+];
+
+export default studyData;
