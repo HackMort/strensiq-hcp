7dd4728f3696343ec3859f06342bbd300cf9144a
elian-dev <geordymemdoza@gmail.com>
2023-04-24 09:11:16 -0600

    feat: :sparkles: Creation - clinical data/Overview page
    The overview page was created (Styles & html)

--- src/pages/clinical-data/data/safety.js
+++ src/pages/clinical-data/data/safety.js
@@ -11,31 +11,59 @@ const data = [
         text: ["Discoloration/hypopigmentation", "11(17)", "1(8)", "8(13)", "5(25)"],
     },
     {
-        text: ["Pain", "10(15)", "1(8)", "7(11)", "4(20)"],
+        text: ["Pain/tenderness", "11(17)", "1(8)", "11(14)", "8(40)"],
     },
     {
-        text: ["Swelling", "9(14)", "1(8)", "6(10)", "4(20)"],
+        text: ["Puritus/itching", "10(15)", "0(0)", "10(13)", "7(35)"],
     },
     {
-        text: ["Induration", "8(12)", "1(8)", "5(8)", "3(15)"],
+        text: ["Swelling", "8(12)", "0(0)", "8(10)", "6(30)"],
     },
     {
-        text: ["Pruritus", "7(11)", "1(8)", "4(6)", "3(15)"],
+        text: ["Induration", "9(14)", "1(8)", "10(13)", "3(15)"],
     },
     {
-        text: ["Burning", "6(9)", "1(8)", "3(5)", "2(10)"],
+        text: ["Macule", "4(6)", "0(0)", "4(5)", "7(35)"],
+    },
+    {
+        text: ["Reaction, not otherwise specified", "6(9)", "1(8)", "7(9)", "4(20)"],
+    },
+    {
+        text: ["Bruising", "8(9)", "0(0)", "6(8)", "4(20)"],
+    },
+    {
+        text: ["Nodule", "2(3)", "0(0)", "6(8)", "4(20)"],
+    },
+    {
+        text: ["Other injection site reactions", "10(15)", "3(23)", "13(17)", "4(20)"],
+    },
+    {
+        text: ["Ectopic calcifications", "3(5)", "0(0)", "13(17)", "4(20)"],
         classModifier: ["mh"],
     },
     {
-        text: ["Tenderness", "6(9)", "1(8)", "3(5)", "2(10)"],
+        text: ["Lipodystrophy", "12(18)", "0(0)", "3(4)", "11(55)"],
+        classModifier: ["mh"],
     },
     {
-        text: ["Numbness", "5(8)", "1(8)", "2(3)", "2(10)"],
+        text: ["Injection site atrophy", "4(6)", "2(15)", "6(8)", "8(40)"],
     },
     {
-        text: ["Paresthesia", "5(8)", "1(8)", "2(3)", "2(10)"],
+        text: ["Injection site hypertrophy", "5(8)", "0(0)", "5(6)", "6(30)"],
     },
-
+    {
+        text: ["Other lipodystrophy", "4(6)", "0(0)", "4(5)", "1(5)"],
+    },
+    {
+        text: ["Hypersensitivy reactions", "7(11)", "2(23)", "10(13)", "2(10)"],
+        classModifier: ["mh"],
+    },
+    {
+        text: ["Vomiting/emisis", "2(3)", "2(15)", "4(5)", "2(10)"],
+    },
+    {
+        text: ["Other hypersensitivity reactions", "6(9)", "2(15)", "8(10)", "2(10)"],
+    },
 ];
 
 export default data;
