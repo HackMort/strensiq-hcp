e8e090e233ad539857d9a346665a40337fdec4e9
elian-dev <geordymemdoza@gmail.com>
2023-04-26 15:13:00 -0600

    feat: :construction: Add content adults page

---
+++ src/pages/clinical-data/data/realWorld.js
@@ -0,0 +1,21 @@
+//Table data
+const Data = [
+    {
+      text: ["Description", "Single-center, observational study of adults with confirmed diagnosis of pediatric-onset hypophosphatasia who received STRENSIQ for >24 months."],
+      classModifier: ["mh"],
+    },
+    {
+        text: ["Duration", "Iterim data reported at 2 years"],
+        classModifier: ["mh", "mb"],
+    },
+    {
+        text: ["N", "14"],
+        classModifier: ["mh"],
+    },
+    {
+        text: ["Endpoints", "Primary: 6-minute walk test (6MWT), timed up-and-go (TUG) test, repeated chair-rise test, Secondary: The Lower Extremity Functional Scale (LEFS)"],
+        classModifier: ["mh"],
+    },
+];
+
+export default Data;
