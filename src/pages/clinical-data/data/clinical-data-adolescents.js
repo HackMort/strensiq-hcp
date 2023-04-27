6fe6e1e5c662f4253c11b1a85a17c4957fc027e4
Jhossymar Contreras <christian.contreras@jdoutstanding.com>
2023-04-27 16:48:58 -0600

    feat: :construction: Implement Table component in adolescents page
    Create js file for table data and implement required HTML structure to add table in Clinical Study Modal

---
+++ src/pages/clinical-data/data/clinical-data-adolescents.js
@@ -0,0 +1,24 @@
+const studyData = [
+  {
+    text: ['Description', 'Prospective, open-label trial and extension in patients with juvenil- or perinatal/infantile-onset hypophosphatasia'],
+    classModifier: ['mh']
+  },
+  {
+    text: ['Duration', '7 years'],
+    classModifier: ['mh', 'mb']
+  },
+  {
+    text: ['N', '13 (9 patients reached adolescence during the studya)'],
+    classModifier: ['mh']
+  },
+  {
+    text: ['Age at inclusion', '6-12 years'],
+    classModifier: ['mh']
+  },
+  {
+    text: ['Endpoints', '<p><strong>Primary:</strong> Hypophosphatasia-related rickets vs historical controls.</p><p><strong>Secondary:</strong> Growth vs historical controls, mobility, strength and agility,b activities of daily living and pain<sup>b</sup></p>'],
+    classModifier: ['mh']
+  }
+]
+
+export default studyData
