8f26afd182aa2d9ccddb3c0182663df6d3e9196f
elian_dev <geordymemdoza@gmail.com>
2023-04-27 14:02:01 -0600

    Feat: page completed

---
+++ src/pages/clinical-data/data/clinicalData.js
@@ -0,0 +1,25 @@
+//Table data
+const ClinicalData = [
+    {
+      text: ["Description", "Multicenter, randomized, open-label, concurrent control trial including adolescent and adults with pediatric-onset hypophosphatasia"],
+      classModifier: ["mh"],
+    },
+    {
+        text: ["Duration", "5 years"],
+        classModifier: ["mh", "mb"],
+    },
+    {
+        text: ["N", "19"],
+        classModifier: ["mh"],
+    },
+    {
+        text: ["Age at inclusion", "13 - 65 years (13 adults,b 6 adolescentsc)"],
+        classModifier: ["mh"],
+    },
+    {
+        text: ["Endpoints", "Primary: Changes in tissue-nonspecific alkaline phosphatase (TNSALP) substrate levels (inorganic pyrophosphate[PPi] and pyridoxal 5'-phosphate [PLP]d,e) Secondary: Bone mineralization, mobilityd"],
+        classModifier: ["mh"],
+    },
+];
+
+export default ClinicalData;
---
+++ src/pages/clinical-data/data/lefsData.js
@@ -0,0 +1,201 @@
+//Table data
+const LefsData = [
+    {
+        text: [
+            "a.Any of your usual work, housework, or school activities", 
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "b.Your usul hobbies, recreational or sporting activities",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "c.Getting into or out of the bath", 
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "d.Walking between rooms", 
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "e.Putting on your shoes or socks", 
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "f.squatting",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "g.Lifting an object, like a bag of groceries, from the floor",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "h.Performing heavy activities around your home",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "i.Performing heavy activities around your home",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "j.Getting into or out of the car",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "k.Walking 2 blocks",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "l.Walking a mile",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "m. Going up or down 10 stairs (about 1 flight of stairs)",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "o.Sitting for 1 hour",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "p.Runing on even ground",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "q.Running on uneven ground",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "r.Making sharp turns while running fast",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "s.Hopping",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "t.Rolling over in bed",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+        ],
+    },
+    {
+        text: [
+            "Score:____/80",
+        ],
+        classModifier: ["mh"],
+    },
+];
+
+export default LefsData;
