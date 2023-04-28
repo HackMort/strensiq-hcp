670779e6d92b38067861048cecaa089982fd5748
Jhossymar Contreras <christian.contreras@jdoutstanding.com>
2023-04-28 10:54:04 -0600

    style: :lipstick: Change styles and add required HTML structure
    Add required HTML structure in some data and change styles to give the same style as in Figma to elements in modal

--- src/pages/clinical-data/data/clinical-data-adolescents.js
+++ src/pages/clinical-data/data/clinical-data-adolescents.js
@@ -1,14 +1,14 @@
 const studyData = [
   {
-    text: ['Description', 'Prospective, open-label trial and extension in patients with juvenil- or perinatal/infantile-onset hypophosphatasia'],
+    text: ['Description', '<p class="limit-width">Prospective, open-label trial and extension in patients with juvenil- or perinatal/infantile-onset hypophosphatasia<p>'],
     classModifier: ['mh']
   },
   {
-    text: ['Duration', '7 years'],
-    classModifier: ['mh', 'mb']
+    text: ['Duration', '<strong>7 years</strong>'],
+    classModifier: ['mh']
   },
   {
-    text: ['N', '13 (9 patients reached adolescence during the studya)'],
+    text: ['N', '<p>13</p><p>(9 patients reached adolescence during the study<sup>a</sup>)</p>'],
     classModifier: ['mh']
   },
   {
