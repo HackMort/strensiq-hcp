853a8501277cf23e83dce7da0c8e84b8f22da261
Jhossymar Contreras <christian.contreras@jdoutstanding.com>
2023-05-02 15:11:42 -0600

    feat: :construction: Add data for infants and children
    Add data for tables in clinical data modal for infants and children pages

---
+++ src/pages/clinical-data/data/clinical-data-children.js
@@ -0,0 +1,24 @@
+const studyData = [
+  {
+    text: ['Description', '<p class="limit-width">Prospective, open-label trial and extension in patients with juvenile-or perinatal/infantile-onset hypophosphatasia</p>'],
+    classModifier: ['mh']
+  },
+  {
+    text: ['Duration', '<strong>7 years</strong>'],
+    classModifier: ['mh']
+  },
+  {
+    text: ['N', '13'],
+    classModifier: ['mh']
+  },
+  {
+    text: ['Age at inclusion', '<p>6-12 years</p><p>(9 patients reached adolescence during the study<sup>a</sup>)</p>'],
+    classModifier: ['mh']
+  },
+  {
+    text: ['Endpoints', '<p><strong>Primary:</strong> Hypophosphatasia-related rickets vs historical controls.</p><p><strong>Secondary:</strong> Growth vs historical controls, mobility, strength and agility,<sup>b</sup> activities of daily living and pain<sup>b</sup></p>'],
+    classModifier: ['mh']
+  }
+]
+
+export default studyData
