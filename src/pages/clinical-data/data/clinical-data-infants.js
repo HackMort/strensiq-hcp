a4ce27dfe86d5cf4b0210b7f76be5c25a0b17498
Jhossymar Contreras <christian.contreras@jdoutstanding.com>
2023-05-13 11:10:15 -0600

    style: :construction: Update content to Figma v3
    Update content in astro page and js for clinical data table to Figma v3

--- src/pages/clinical-data/data/clinical-data-infants.js
+++ src/pages/clinical-data/data/clinical-data-infants.js
@@ -1,11 +1,19 @@
 const studyData = [
   {
-    text: ['Description', '<p class="limit-width">Prospective, single-arm trial and extension in patients with severe perinatal/infantile-onset hypophosphatasia</p>', '<p class="limit-width">Prospective, open-label trial in patients with perinatal. infantile-onset hypophosphatasia</p>'],
+    text: ['Description', '<p>Prospective, single-arm trial and extension in patients with severe perinatal/infantile-onset hypophosphatasia</p>', '<p>Prospective, open-label, multicenter, single-arm trial in patients with perinatal/infantile onset HPP</p>'],
     classModifier: ['mh']
   },
   {
     text: ['Duration', '<strong>7 years</strong>', '<strong>6 years</strong>'],
     classModifier: ['mh']
+  },
+  {
+    text: ['Primary treatment phase', '6 months', '1-6 years'],
+    classModifier: []
+  },
+  {
+    text: ['Extension', '6.5 years', '1-6 years'],
+    classModifier: []
   },
   {
     text: ['N', '11', '69'],
@@ -16,7 +24,7 @@ const studyData = [
     classModifier: ['mh']
   },
   {
-    text: ['Endpoints', '<p><strong>Primary:</strong> Change in hypophosphatasia-related rickets at 6 months</p><p><strong>Secondary:</strong> Growth, survival, and invasive ventilation-free survival vs untreated historical controls'],
+    text: ['Endpoints', '<p><strong>Primary:</strong> Change in skeletal manifestations, including rickets, at 6 months, as measured by the RGI-C scale</p><p><strong>Secondary:</strong> Growth, survival, and invasive ventilation-free survival vs untreated historical controls'],
     classModifier: ['mh', 'join']
   }
 ]
