d5f907892f943cfb6a133c97cb1659d0e1010b05
Jhossymar Contreras <christian.contreras@jdoutstanding.com>
2023-05-15 16:26:52 -0600

    feat: :lipstick: Update content to Figma  v3
    Update content of all sections, table in modal and references according to Figma v3

--- src/pages/clinical-data/data/clinical-data-children.js
+++ src/pages/clinical-data/data/clinical-data-children.js
@@ -1,6 +1,6 @@
 const studyData = [
   {
-    text: ['Description', '<p class="limit-width">Prospective, open-label trial and extension in patients with juvenile-or perinatal/infantile-onset hypophosphatasia</p>'],
+    text: ['Description', '<p class="limit-width">Prospective, open-label trial and extension in patients with juvenile- or perinatal/infantile-onset hypophosphatasia</p>'],
     classModifier: ['mh']
   },
   {
@@ -8,15 +8,23 @@ const studyData = [
     classModifier: ['mh']
   },
   {
-    text: ['N', '13'],
+    text: ['Primary treatment phase', '6 months'],
+    classModifier: ['normal']
+  },
+  {
+    text: ['Extension', '6.5 years'],
+    classModifier: ['normal']
+  },
+  {
+    text: ['N', '<p>13</p><p>(9 patients reached adolescents during the study<sup>a</sup>)</p>'],
     classModifier: ['mh']
   },
   {
-    text: ['Age at inclusion', '<p>6-12 years</p><p>(9 patients reached adolescence during the study<sup>a</sup>)</p>'],
+    text: ['Age at inclusion', '6-12 years'],
     classModifier: ['mh']
   },
   {
-    text: ['Endpoints', '<p><strong>Primary:</strong> Hypophosphatasia-related rickets vs historical controls.</p><p><strong>Secondary:</strong> Growth vs historical controls, mobility, strength and agility,<sup>b</sup> activities of daily living and pain<sup>b</sup></p>'],
+    text: ['Endpoints', '<p><strong>Primary:</strong> Evaluation of changes in skeletal manifestations of hypophosphatasia versus historical controls at 6 months using the RGI-C</p><p><strong>Secondary:</strong> Growth vs historical controls, mobility, strength and agility,<sup>b</sup> activities of daily living and pain<sup>b</sup></p>'],
     classModifier: ['mh']
   }
 ]
