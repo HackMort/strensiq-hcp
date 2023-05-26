2c4c5c6e114f06da523ca38c7b909ae0016ef096
elian-dev <geordymemdoza@gmail.com>
2023-05-26 13:36:05 -0600

    fix: :bug: Fix content and style clinical data / adults page

--- src/pages/clinical-data/data/realWorld.js
+++ src/pages/clinical-data/data/realWorld.js
@@ -1,25 +1,31 @@
 // Table data
 const Data = [
   {
-    text: ['Description', 'Single-center, observational study of adults with confirmed diagnosis of pediatric-onset hypophosphatasia who received STRENSIQ for ≤ 24 months.'],
-    classModifier: ['mh']
+    text: [
+      "Description",
+      "Single-center, observational, retrospective and prospective study of adults with confirmed diagnosis of pediatric-onset hypophosphatasia who received STRENSIQ for ≥24 months",
+    ],
+    classModifier: ["mh"],
   },
   {
-    text: ['Duration', '<strong>Iterim data reported at 2 years</strong>'],
-    classModifier: ['mh']
+    text: ["Duration", "<strong>Iterim data reported at 2 years</strong>"],
+    classModifier: ["mh"],
   },
   {
-    text: ['N', '14'],
-    classModifier: ['mh']
+    text: ["N", "14"],
+    classModifier: ["mh"],
   },
   {
-    text: ['Age at inclusion', '≥18years'],
-    classModifier: ['mh']
+    text: ["Age at inclusion", "≥18years"],
+    classModifier: ["mh"],
   },
   {
-    text: ['Endpoints', '<strong>Key primary:</strong> 6-minute walk test (6MWT), timed up-and-go (TUG) test, repeated chair-rise test <br> <strong>Key secondary:</strong> The Lower Extremity Functional Scale (LEFS)'],
-    classModifier: ['mh']
-  }
-]
+    text: [
+      "Endpoints",
+      "<strong>Key primary:</strong> 6-minute walk test (6MWT), timed up-and-go (TUG) test, repeated chair rise test <br> <strong>Key secondary:</strong> The Lower Extremity Functional Scale (LEFS)",
+    ],
+    classModifier: ["mh"],
+  },
+];
 
 export default Data
